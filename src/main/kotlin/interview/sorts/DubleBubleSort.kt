0f2f94613239c7a2ce1c8f73fbf9bbfcfbc128db
unknown <snovaodin@gmail.com>
2023-04-02 10:27:50 +0300

    Duble Buble Sort

--- src/main/kotlin/interview/sorts/DubleBubleSort.kt
+++ src/main/kotlin/interview/sorts/DubleBubleSort.kt
@@ -1,24 +1,37 @@
 package interview.sorts
 
 fun main() {
-    val list = mutableListOf(64, 42, 73, 41, 31, 53, 1, 24, 57, 42, 74, 55, 36, 13, 82, 99)
+    val list = mutableListOf(42, 64, 73, 31, 53, 24, 82, 36, 13, 57, 35, 1, 54)
 
-    var count = 0
-    println("%04d".format(++count) + " -> $list")
 
-    var isSorted = false
-    while (!isSorted) {
-        isSorted = true
-        for (i in (list.lastIndex-1)downTo 0) {
-            if (list[i+1] > list[i]) {
-                val temp = list[i]
-                list[i] = list[i+1]
-                list[i+1] = temp
-                isSorted = false
+//    var count = 0
+//    println("%04d".format(0) + " -> $list")
+
+    var sub = 0
+    var sorted = false
+    while (!sorted) {
+        sorted = true
+
+        val left = 0 + sub
+        val right = list.lastIndex - 1 - sub++
+        for (i in right downTo left) {
+
+            if (list[i] < list[i+1]) {
+                val max = list[i+1]
+                list[i+1] = list[i]
+                list[i] = max
+                sorted = false
             }
-        }
-        println("%04d".format(++count) + " -> $list")
-    }
 
-    println("step end -> $list")
+            val rightIndex = list.lastIndex-i
+            if (list[rightIndex-1] < list[rightIndex]) {
+                val min = list[rightIndex-1]
+                list[rightIndex-1] = list[rightIndex]
+                list[rightIndex] = min
+                sorted = false
+            }
+            //println("%04d".format(++count) + " -> $list")
+        }
+        //println("=================================================")
+    }
 }
